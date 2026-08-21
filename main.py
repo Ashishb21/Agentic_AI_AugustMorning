c80c4a92229915d9b8ddb63c31cdff58bcb19cf2
ashishb21 <ashishb21@gmail.com>
2026-08-21 07:26:47 +0530

    classes

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.mydata import Emp
+
+emp1 = Emp("suresh","12")
+print(emp1.get_details())
---
+++ src/mydata.py
@@ -0,0 +1,9 @@
+
+class Emp:
+
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def get_details(self):
+        return self.name, self.age
